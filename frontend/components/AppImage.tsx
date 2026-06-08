f1f05d37640c28d687d170b2a03dbc7b032bc8de
ChengjinLii <2731938007@qq.com>
2026-06-08 13:33:13 +0800

    补充图片异步解码默认值

--- frontend/components/AppImage.tsx
+++ frontend/components/AppImage.tsx
@@ -4,11 +4,11 @@ export type AppImageProps = Omit<ImgHTMLAttributes<HTMLImageElement>, 'alt'> & {
   alt: string;
 };
 
-const AppImage = forwardRef<HTMLImageElement, AppImageProps>(function AppImage(props, ref) {
+const AppImage = forwardRef<HTMLImageElement, AppImageProps>(function AppImage({ decoding = 'async', ...props }, ref) {
   // Many StudyHub images are runtime URLs, captcha data URLs, or user-uploaded assets.
   // Keep one shared rendering entry so future image policy changes happen in one place.
   /* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */
-  return <img ref={ref} {...props} />;
+  return <img ref={ref} decoding={decoding} {...props} />;
 });
 
 export default AppImage;
